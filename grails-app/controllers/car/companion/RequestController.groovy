1dd0ff7c11fa783ea2f9c3a21d601c75b5b61795
Leonid Konyaev <liosson@gmail.com>
2012-12-11 01:44:41 +0700

    request controller now works through GET

--- grails-app/controllers/car/companion/RequestController.groovy
+++ grails-app/controllers/car/companion/RequestController.groovy
@@ -4,29 +4,70 @@ class RequestController {
 
     def index() { }
 
-    def addRequest(User s, User d) {
-        new Request(src: s, dest: d, status: Request.NEW, date: new Date()).save(failOnError: true)
+    def addRequest() {
+        User user = (User)session.getAttribute("user")
+        if (user != null) {
+            User destUser = User.findById(Long.parseLong(params['destId'].toString()));
+            if (destUser != null) {
+                new Request(src: user, dest: destUser, status: Request.NEW, date: new Date()).
+                        save(failOnError: true)
+            }
+        }
     }
 
     // find all requests that comes to user from other users
-    def listIncome(User user, Integer status) {
-        def resultList = Request.findAllByDestAndStatus(user, status);
-        [incomeRequests: resultList]
+    def listIncome(Integer status) {
+        User user = (User)session.getAttribute("user")
+        def resultList = []
+        if (user != null) {
+            resultList = Request.findAllByDestAndStatus(
+                user,
+                Integer.parseInt(params['status'].toString())
+            );
+        }
+        render(contentType:"text/json") {
+            [incomeRequests: resultList]
+        }
     }
 
     // final all requests that were sent by user
-    def listOutcome(User user, Integer status) {
-        def resultList = Request.findAllBySrcAndStatus(user, status);
-        [outcomeRequests: resultList]
+    def listOutcome() {
+        User user = (User)session.getAttribute("user")
+        def resultList = []
+        if (user != null) {
+            resultList = Request.findAllBySrcAndStatus(
+                user,
+                Integer.parseInt(params['status'].toString())
+            );
+        }
+        render(contentType:"text/json") {
+            [outcomeRequests: resultList]
+        }
     }
 
-    def acceptRequest(Request request) {
-        request.status = Request.ACCEPTED;
-        request.save(failOnError: true);
+    def acceptRequest() {
+        Request request = Request.findById(Long.parseLong(params['requestId'].toString()))
+        def res = false
+        if (request != null) {
+            request.status = Request.ACCEPTED;
+            request.save(failOnError: true);
+            res = true
+        }
+        render(contentType:"text/json") {
+            [result: res]
+        }
     }
 
-    def declineRequest(Request request) {
-        request.status = Request.DECLINED;
-        request.save(failOnError: true);
+    def declineRequest() {
+        Request request = Request.findById(Long.parseLong(params['requestId'].toString()))
+        def res = false
+        if (request != null) {
+            request.status = Request.DECLINED;
+            request.save(failOnError: true);
+            res = true
+        }
+        render(contentType:"text/json") {
+            [result: res]
+        }
     }
 }
